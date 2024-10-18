48abb9cd580fff5ce78eadf9ff156fdf8de16fff
Angel Hernandez <angeldariaux@gmail.com>
2024-10-17 18:20:15 -0600

    imprimiendo el id de las listas y creando copias de las listas

--- curso_de_python/listas_diccionarios.py
+++ curso_de_python/listas_diccionarios.py
@@ -12,3 +12,18 @@ print(numbers)
 
 del numbers[:2]
 print(numbers)
+
+
+a = [1, 2, 3, 4, 5]
+b = a
+print(a)
+print(b)
+del a[0]
+print(id(a))  # imprime el identificador del puntero de esta lista
+print(id(b))
+print(a)
+c = a[:]  # copia los valores del array
+print(id(a))
+print(id(b))
+print(c)
+print(id(c))
