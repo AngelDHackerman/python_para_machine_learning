ce93684d0bf93328c27c6c7f5d39dbfac458826a
Angel Hernandez <angeldariaux@gmail.com>
2024-10-17 18:15:20 -0600

    created git ignore and added first lines of code

---
+++ curso_de_python/listas_diccionarios.py
@@ -0,0 +1,14 @@
+
+to_do = ["iremos a casa", 
+         "hora de comer", 
+         "el museo",
+         "volver a casa"]
+print(to_do)
+numbers = [1, 2, 3, 4, "Cinco"]
+print(numbers)
+
+del numbers[-1]
+print(numbers)
+
+del numbers[:2]
+print(numbers)
---
+++ curso_de_python/python_clase_1.py
@@ -0,0 +1,10 @@
+# Uso de "end"
+
+print("Nunca", end=" ")
+print("pares de aprender")
+
+# Uso de format()
+
+frase = "Nunca pares de aprender"
+author = "Platzi"
+print("Frase: {}, Autor: {}".format(frase, author))
